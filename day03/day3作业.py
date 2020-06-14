9fb6c141bd424549e12dd8b4addce4c502b99eb8
PG杰尼龟 <1095845472@qq.com>
2020-06-14 15:55:58 +0800

    day3作业111

--- day03/day3作业.py
+++ day03/day3作业.py
@@ -1,39 +1,39 @@
 # 1、如果输⼊“1，2，3，4”，显示正⽉，⼆⽉，三⽉，四⽉…  数字与汉字⽉份转换
-# str_month = input("输入数字，即可转化成为汉字月份")
-# if str_month == '1':
-#     print("一月")
-# elif str_month == '2':
-#     print("二月")
-# elif str_month == '3':
-#     print("三月")
-# elif str_month == '4':
-#     print("四月")
-# elif str_month == '5':
-#     print("五月")
-# elif str_month == '6':
-#     print("六月")
-# elif str_month == '7':
-#     print("七月")
-# elif str_month == '8':
-#     print("八月")
-# elif str_month == '9':
-#     print("九月")
-# elif str_month == '10':
-#     print("十月")
-# elif str_month == '11':
-#     print("十一月")
-# else:
-#     print("十二月")
+str_month = input("输入数字，即可转化成为汉字月份")
+if str_month == '1':
+    print("一月")
+elif str_month == '2':
+    print("二月")
+elif str_month == '3':
+    print("三月")
+elif str_month == '4':
+    print("四月")
+elif str_month == '5':
+    print("五月")
+elif str_month == '6':
+    print("六月")
+elif str_month == '7':
+    print("七月")
+elif str_month == '8':
+    print("八月")
+elif str_month == '9':
+    print("九月")
+elif str_month == '10':
+    print("十月")
+elif str_month == '11':
+    print("十一月")
+else:
+    print("十二月")
 
 
 # 2、对商品猜价格：设定⼀个神秘价格。你从控制台输⼊信息价格，让⽤户猜，只提示，猜⼤了，猜⼩了，猜对。
-# str_price = input("猜出商品的价格")
-# if str_price < '125':
-#     print("猜小了")
-# elif str_price > '125':
-#     print("猜大了")
-# else:
-#     print("猜对了")
+str_price = input("猜出商品的价格")
+if str_price < '125':
+    print("猜小了")
+elif str_price > '125':
+    print("猜大了")
+else:
+    print("猜对了")
 
 # 3、简单描述变量从创建，引⽤到销毁的过程。
 # 当变量被赋值的时候，此时就是一个变量创建的过程。
